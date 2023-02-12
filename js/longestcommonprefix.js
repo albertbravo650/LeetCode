2a4c33df561345ca0cd183f0a779cca2865b8427
Albert B <abduarte20@gmail.com>
2023-02-12 11:57:54 -0800

    next commit

--- js/longestcommonprefix.js
+++ js/longestcommonprefix.js
@@ -1,7 +1,9 @@
 function longestCommonPrefix(strs) {
     let lgc = "";
     for(i=0; i<strs.length; i++) {
-        
+        console.log(strs[i])
     }
     return lgc;
 }
+
+longestCommonPrefix(["flower", "flow", "flight"]);
