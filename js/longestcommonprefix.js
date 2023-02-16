702cf32acf569c4b30be0121e3945df147d47df7
Albert B <abduarte20@gmail.com>
2023-02-15 23:22:00 -0800

    1st solution

--- js/longestcommonprefix.js
+++ js/longestcommonprefix.js
@@ -1,20 +1,17 @@
 function longestCommonPrefix(strs) {
     let lgc = "";
     let firstStr = strs[0];
-    console.log(firstStr);
     // loop through the arrray of strings
     for(i=1; i<strs.length; i++) {
-        console.log(strs[i]);
-        for(j=0; j<strs[i].length; j++) {
-            // console.log(strs[i][j]);
-            if(strs[i][j]===firstStr[j]) {
-                lgc += strs[i][j];
-            }
-
+        // console.log(strs[i]);
+        if(!strs[i].startsWith(firstStr)) {
+            firstStr = firstStr.slice(0, -1);
+        } else {
+            i++;
         }
     }
     // return the longest common prefix
-    console.log(lgc);
+    console.log(firstStr);
     return lgc;
 }
 
