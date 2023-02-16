0470904c0ce5828beb09749082a123acda617fb8
Albert B <abduarte20@gmail.com>
2023-02-15 19:36:58 -0800

    next commit

--- js/longestcommonprefix.js
+++ js/longestcommonprefix.js
@@ -1,10 +1,20 @@
 function longestCommonPrefix(strs) {
     let lgc = "";
+    let firstStr = strs[0];
+    console.log(firstStr);
     // loop through the arrray of strings
-    for(i=0; i<strs.length; i++) {
-        console.log(strs[i])
+    for(i=1; i<strs.length; i++) {
+        console.log(strs[i]);
+        for(j=0; j<strs[i].length; j++) {
+            // console.log(strs[i][j]);
+            if(strs[i][j]===firstStr[j]) {
+                lgc += strs[i][j];
+            }
+
+        }
     }
     // return the longest common prefix
+    console.log(lgc);
     return lgc;
 }
 
