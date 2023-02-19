522e67f9b2e6c606d389ba884f66e2fa26f21b65
Albert B <abduarte20@gmail.com>
2023-02-19 00:17:47 -0800

    next commit

--- js/practice2.js
+++ js/practice2.js
@@ -1,3 +1,29 @@
-var arr1 = [5, 5, 7, 2];
-var num1 = arr1.pop();
-console.log(num1);
+// var arr1 = [5, 5, 7, 2];
+// var num1 = arr1.pop();
+// console.log(num1);
+var day = '';
+switch(new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+}
+
+console.log(day);
