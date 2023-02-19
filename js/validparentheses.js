d19c7d705026b7d23e8f506eac786b0ffcfc4acb
Albert B <abduarte20@gmail.com>
2023-02-18 22:49:41 -0800

    next commit

--- js/validparentheses.js
+++ js/validparentheses.js
@@ -5,6 +5,7 @@ function isValid(s) {
     for(i=0; i<s.length; i++) {
         // console.log(s[i]);
         if(s[i+1]) {
+            // INCORRECT SOLUTION, DOES NOT SOLVE FOR NESTED PARENTHESES eg.([{}])
             temp1 = s[i];
             temp2 = s[i+1];
             console.log(temp1, temp2);
