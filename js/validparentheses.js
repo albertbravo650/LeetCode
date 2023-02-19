2fe7f10c5a1e9c23e41515c446d05a8eadd3ae71
Albert B <abduarte20@gmail.com>
2023-02-18 22:32:46 -0800

    next commit

--- js/validparentheses.js
+++ js/validparentheses.js
@@ -1,9 +1,36 @@
 function isValid(s) {
+    let valid = false;
+    let temp1 = "";
+    let temp2 = "";
     for(i=0; i<s.length; i++) {
-        console.log(s[i]);
+        // console.log(s[i]);
+        if(s[i+1]) {
+            temp1 = s[i];
+            temp2 = s[i+1];
+            console.log(temp1, temp2);
+            if(temp1==="(" && temp2===")") {
+                valid = true;
+                i++;
+            }
+            else if(temp1==="[" && temp2==="]") {
+                valid = true;
+                i++;
+            }
+            else if(temp1==="{" && temp2==="}") {
+                valid = true;
+                i++;
+            }
+            else {
+                valid = false;
+                console.log(valid);
+                return false;
+            }
+        }
     }
+    console.log(valid);
+    return valid;
 }
 
 isValid("()");
-// isValid("()[]{}");
-// isValid("(]");
+isValid("()[]{}");
+isValid("(]");
