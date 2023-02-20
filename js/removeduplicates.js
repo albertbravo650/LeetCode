1dbfe50c1d7ab987c9e353e0cec9b421d81e778f
Albert B <abduarte20@gmail.com>
2023-02-20 00:41:58 -0800

    1st commit

---
+++ js/removeduplicates.js
@@ -0,0 +1,14 @@
+function removeDuplicates(nums) {
+    let k = nums.length;
+    console.log(k);
+    let j = nums[0];
+    for(i=1; i<nums.length; i++) {
+        if(j===nums[i]) {
+            
+        }
+    }
+    return k;
+}
+
+var nums = [1,1,2];
+var k = removeDuplicates(nums);
