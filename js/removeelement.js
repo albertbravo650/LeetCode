8a11c03aece9de38a42cc6a914bc1900d2e3ac6f
Albert B <abduarte20@gmail.com>
2023-02-23 22:59:45 -0800

    next commit

--- js/removeelement.js
+++ js/removeelement.js
@@ -1,6 +1,17 @@
 function removeElement(nums, val) {
+    for(i=0; i<nums.length; i++) {
+        if(nums[i]===val) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    console.log(nums);
+    console.log(nums.length);
     return nums.length;
 }
 
 var nums1 = [3,2,2,3], val1 = 3;
 var k1 = removeElement(nums1, val1);
+
+var nums2 = [0,1,2,2,3,0,4,2], val2 = 2;
+var k2 = removeElement(nums2, val2);
