f3ef075ede4190d41aa59fa11413f24f753091fe
Albert B <abduarte20@gmail.com>
2024-01-02 12:56:13 -0800

    next commit

---
+++ js/1TwoSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var sumNums = [];
+    for (i=0; i < nums.length; i++) {
+        for (x=0; x < nums.length; x++) {
+            if ((nums[i]+nums[x]) == target && i!=x) {
+            sumNums.push(i)
+            }
+        }
+    }
+    return sumNums;
+};
