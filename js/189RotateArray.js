8854a13f8cac451896f18a0571ffb0ae2ef03265
Albert B <abduarte20@gmail.com>
2024-03-27 17:57:58 -0700

    next commit

--- js/189RotateArray.js
+++ js/189RotateArray.js
@@ -4,22 +4,26 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-  for(let i=0; i<k; i++) {
-    for(let j=0; j<nums.length; j++) {
-      if(!nums[j+1]) {
-        nums[0] = nums[nums.length-1]
-      }
-      else {
-        let temp = nums[j+1];
-        nums[j+1] = nums[j];
-        nums[j] = temp;
-      }
-    }
-  }
-  console.log(nums);
+  k = k % nums.length;
+  // console.log(20 % 7);
+  reverseArray(nums, 0, nums.length - 1); // reverse entire array
+  reverseArray(nums, 0, k - 1); // reverse first k times elements
+  reverseArray(nums, k, nums.length - 1); // reverse remaining elements
   return nums;
 };
 
+let reverseArray = function(nums, start, end) {
+  // console.log(nums);
+  while(start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+}
+
 const nums = [1,2,3,4,5,6,7];
 const k = 3;
+// console.log(nums);
 console.log(rotate(nums, k));
