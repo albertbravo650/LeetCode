4d0639553dcf041f2653133d7b450afdb76fbc20
Albert B <abduarte20@gmail.com>
2024-03-28 23:57:57 -0700

    next commit

---
+++ js/121BestTimetoBuyandSellStock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let maxP = 0;
+  let minimum = Number.MAX_VALUE;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minimum) {
+      minimum = prices[i];
+    } else if (prices[i] - minimum > maxP) {
+      maxP = prices[i] - minimum;
+    }
+  }
+  return maxP;
+};
+
+let prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
